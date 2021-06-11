42a8351729408a1de45262f1923f5e2836c54367
kds <kds2048@naver.com>
2021-06-11 17:24:10 +0900

    2021-06-11

--- workspace/02_DI/src/main/java/ex01_inject/SelectListCommand.java
+++ workspace/02_DI/src/main/java/ex01_inject/SelectListCommand.java
@@ -1,6 +1,14 @@
 package ex01_inject;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+	@Inject
+	
+	1. 객체의 타입(<bean class="">)이 일치하는 객체를 자동으로 주입한다.
+	2. 필드, 생성자, setter를 대상으로 한다.
+*/
 
 public class SelectListCommand {
 
@@ -34,15 +42,22 @@ public class SelectListCommand {
 	// field
 	private Dao dao;
 	
+	// constructor
+	public SelectListCommand() {}
+	
 	// setter
 	@Inject
+	@Named("dao1")  // <bean id="dao1"> 인 bean을 생성하라.
 	public void setDao(Dao dao) {
 		this.dao = dao;
+	}
+	// getter
+	public Dao getDao() {
+		return dao;
 	}
 	
 	public void execute() {
 		dao.selectList();  // dao가 자동으로 주입되지 않았다면 NullPointerException 발생
 	}
-	
 	
 }
